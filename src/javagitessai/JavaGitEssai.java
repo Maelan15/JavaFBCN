823bb529916264acfc9c7bea15482f84176a67db
Rabelais <Rabelais@DESKTOP-ELJAVLE>
2021-05-03 09:53:50 +0200

    j'aime jul

---
+++ src/javagitessai/JavaGitEssai.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagitessai;
+
+/**
+ *
+ * @author Rabelais
+ */
+public class JavaGitEssai
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // Remarche pour essaie
+    }
+    
+}
